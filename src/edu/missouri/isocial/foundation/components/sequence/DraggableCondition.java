0c8302c30d9d6bfe35fc850cc7f8ef332a55d2b2
JagWire <babiuchr@missouri.edu>
2013-04-18 12:26:10 -0500

    Cleaned up DraggableCondition code. - removed useless comments - pulled Connector variables out of constructor and into private fields.

--- src/edu/missouri/isocial/foundation/components/sequence/DraggableCondition.java
+++ src/edu/missouri/isocial/foundation/components/sequence/DraggableCondition.java
@@ -18,55 +18,46 @@ import java.awt.geom.GeneralPath;
  * @author Ryan
  */
 public class DraggableCondition extends DraggableJPanel {
-    
+
     private final static int SCALE = 150;
+    private Connector bottom;
+    private Connector top;
+    private Connector right;
+
     public DraggableCondition(EditorApplication editor) {
         super(editor);
-        
-        Connector bottom = new Connector(editor, this);
+
+        bottom = new Connector(editor, this);
         add(bottom);
-        bottom.setLocation(SCALE/2 - (bottom.getWidth()/2), SCALE - (bottom.getHeight()/2));
+        bottom.setLocation(SCALE / 2 - (bottom.getWidth() / 2), SCALE - (bottom.getHeight() / 2));
         bottom.setVisible(true);
-        
-        Connector top = new Connector(editor, this);
+
+        top = new Connector(editor, this);
         add(top);
-        top.setLocation(SCALE/2 -(top.getWidth())/2, 0);
+        top.setLocation(SCALE / 2 - (top.getWidth()) / 2, 0);
         top.setVisible(true);
-        
-        Connector right = new Connector(editor, this);
+
+        right = new Connector(editor, this);
         add(right);
-        right.setLocation(SCALE - right.getWidth()/2, SCALE/2 - (right.getWidth()/2));
+        right.setLocation(SCALE - right.getWidth() / 2, SCALE / 2 - (right.getWidth() / 2));
         right.setVisible(true);
     }
-    
+
     @Override
     protected void paintComponent(Graphics g1) {
-        Graphics2D g = (Graphics2D)g1;
-//        Rectangle r = new Rectangle(0, 0, 100, 100);
+        Graphics2D g = (Graphics2D) g1;
 
-        g.setColor(Color.red);
-//        g.translate(r.x+(r.width/2), r.y+(r.height/2));
-//        g.rotate(Math.toRadians(90));
-//        g.drawRect(0,0, 199, 199);
-        
         GeneralPath path = new GeneralPath();
-        path.moveTo(SCALE/2, 0);
-        path.lineTo(SCALE, SCALE/2);
-        path.lineTo(SCALE/2, SCALE);
-        path.lineTo(0, SCALE/2);
-        path.lineTo(SCALE/2, 0);
-//        g.scale(2, 2);
-          
+        path.moveTo(SCALE / 2, 0);
+        path.lineTo(SCALE, SCALE / 2);
+        path.lineTo(SCALE / 2, SCALE);
+        path.lineTo(0, SCALE / 2);
+        path.lineTo(SCALE / 2, 0);
+
         g.setColor(Color.RED);
         g.fill(path);
         g.setColor(Color.BLACK);
-        g.draw(path);    
-//        g.dispose();
-//        
-//        g = (Graphics2D)g.create();
-        
-        
+        g.draw(path);
 
-        
     }
 }
